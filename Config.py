87d1f61e203e1d368f8ea463d385f2b869f6ccb9
the-jude10 <tarkighirjude29@gmail.com>
2025-07-25 11:05:24 +0100

    Initial commit
    Python file for the inventory system

---
+++ Config.py
@@ -0,0 +1,6 @@
+class Config:
+    MYSQL_HOST = 'localhost'
+    MYSQL_USER = 'your_username'
+    MYSQL_PASSWORD = 'your_password'
+    MYSQL_DB = 'inventory_db'
+    SECRET_KEY = 'your_secret_key'
